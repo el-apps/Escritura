52f4cf8a637575d40392b7a8c69df67172a284fb
Addison Emig <addison@kwila.cloud>
2025-08-21 06:52:08 -0400

    fix: gradle config

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,12 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+val keystorePropertiesFile = rootProject.file("keystore.properties")
+val keystoreProperties = Properties()
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -30,16 +39,17 @@ android {
     }
 
     signingConfigs {
-       release {
-           keyAlias = keystoreProperties['keyAlias']
-           keyPassword = keystoreProperties['keyPassword']
-           storeFile = keystoreProperties['storeFile'] ? file(keystoreProperties['storeFile']) : null
-           storePassword = keystoreProperties['storePassword']
+       create("release") {
+           storeFile = file(keystoreProperties["storeFile"])
+           storePassword = keystoreProperties["storePassword"] as String
+           keyAlias = keystoreProperties["keyAlias"] as String
+           keyPassword = keystoreProperties["keyPassword"] as String
        }
     }
     buildTypes {
        release {
-           signingConfig = signingConfigs.release
+           isMinifyEnabled = true
+           signingConfig = signingConfigs["release"]
        }
     }
 }
