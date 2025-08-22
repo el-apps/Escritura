cd1137d2ff7b179d7c115e5e6f04727f53ec3296
Addison Emig <addison@kwila.cloud>
2025-08-22 09:42:12 -0400

    feat: sharing works now

--- lib/share_dialog.dart
+++ lib/share_dialog.dart
@@ -1,5 +1,6 @@
 import 'package:escritura/practice_result.dart';
 import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
 
 class ShareDialog extends StatelessWidget {
   const ShareDialog({super.key, required this.memorizationResults});
@@ -7,30 +8,47 @@ class ShareDialog extends StatelessWidget {
   // TODO: fetch these from the DB
   final List<MemorizationResult> memorizationResults;
 
+  get _shareContent => [
+    memorizationResults
+        // TODO: get prettier string from bible service
+        .map((result) => '${result.ref} ${result.attempts == 1 ? "🎉" : "✅"}')
+        .join('\n'),
+    // TODO: add results from other parts of the app
+  ].where((section) => section.isNotEmpty).join('----------\n');
+
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       title: Text('Share Your Progress'),
       content: Column(
         mainAxisSize: MainAxisSize.min,
+        spacing: 4,
         children: [
           Text(
             'Daily sharing your results with others is a great way to practice accountability!',
           ),
-          // TODO: allow the user to determine what data they want to share
-          //       for example, they may want to share their memorization but
-          //       not their reference identification results
-          for (final (:ref, :attempts) in memorizationResults)
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              // TODO: get prettier string from bible service
-              children: [Text('$ref'), Text(attempts == 1 ? '🎉' : '✅')],
-            ),
+          Divider(),
+          Text(_shareContent),
         ],
       ),
+
       actions: [
-        // TODO: add share button
+        TextButton(
+          onPressed: () => Navigator.of(context).pop(),
+          child: Text('Cancel'),
+        ),
+        if (_shareContent.isNotEmpty)
+          TextButton(onPressed: () => _share(context), child: Text('Share')),
       ],
     );
+  }
+
+  _share(BuildContext context) async {
+    await SharePlus.instance.share(
+      ShareParams(text: 'Escritura Daily Results\n\n$_shareContent'),
+    );
+    if (context.mounted) {
+      Navigator.of(context).pop();
+    }
   }
 }
